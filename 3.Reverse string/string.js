efd50b4d07674e213507a5a48ddfb3a04f728c7a
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-08-27 08:39:11 +0530

    Reverse string

---
+++ 3.Reverse string/string.js
@@ -0,0 +1,13 @@
+// Implement a function that reverses a string without using the built-in reverse method
+
+function reverseString(str) {
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+}
+
+const original = "Hello, world!";
+const reversed = reverseString(original);
+console.log(reversed);
