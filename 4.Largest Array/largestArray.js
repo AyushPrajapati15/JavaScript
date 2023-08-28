c9712d60dccdbf329ccd16de54f41209d588cade
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-08-28 19:35:02 +0530

    Largest element in array

--- 4.Largest Array/largestArray.js
+++ 4.Largest Array/largestArray.js
@@ -1,15 +1,14 @@
 // program to find the largest element in an array.
 function large(arr) {
-    let largest = arr[0];
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] > largest) {
-            largest = arr[i];
-        }
+  let largest = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
     }
-    return largest;
+  }
+  return largest;
 }
-    
 
-let arr = [1, 5, 2, 4, 3, 8, 9]
+let arr = [1, 5, 2, 4, 3, 8, 9];
 // large(arr);
 console.log(large(arr));
