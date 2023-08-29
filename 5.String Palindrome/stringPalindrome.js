5fd03aa547614b9fea5dfb87a3e0ddf25e355525
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-08-29 13:01:02 +0530

    String is palinfrome or not

--- 5.String Palindrome/stringPalindrome.js
+++ 5.String Palindrome/stringPalindrome.js
@@ -1,13 +1,10 @@
 // function to check if a string is a palindrome or not
 function isPalindrome(string) {
-    
-        const cleanedStr = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-        
-    
-        return cleanedStr === cleanedStr.split('').reverse().join('');
-      }
-      
-      
-      console.log(isPalindrome("racecar"));
-      console.log(isPalindrome("hello")); 
-      console.log(isPalindrome("A man, a plan, a canal, Panama"));
+  const cleanedStr = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+  return cleanedStr === cleanedStr.split("").reverse().join("");
+}
+
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("hello"));
+console.log(isPalindrome("A man, a plan, a canal, Panama"));
