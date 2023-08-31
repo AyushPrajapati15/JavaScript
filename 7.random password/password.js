fe281171881976413b9eaadf563afaf623401d63
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-08-31 22:33:17 +0530

    Random password

---
+++ 7.random password/password.js
@@ -0,0 +1,16 @@
+function generateRandomPassword(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+";
+    let password = "";
+  
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+  
+    return password;
+  }
+  
+  const passwordLength = 12; // You can change this to the desired password length
+  const randomPassword = generateRandomPassword(passwordLength);
+  console.log(randomPassword);
+  
