9438557ecb822045f9461438241bce334f069c93
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-09-03 21:22:41 +0530

    Grade checker

--- 9.Grade/Grade_checker.js
+++ 9.Grade/Grade_checker.js
@@ -1,23 +1,17 @@
 // Create a program that takes a student's score as input and then displays their corresponding letter grade (A, B, C, D, or F) using an if...else statement.
 
 function score(num) {
-    if (num >= 90) {
-        return "Grade A";
-    }
-    else if (num >= 80 && num < 90) {
-        return "Grade B";
-    }
-    else if (num >= 70 && num < 80) {
-        return "Grade C";
-    }
-    else if (num >= 60 && num <= 70) {
-        return "Grade D";
-    }
-    else if (num >= 50 && num <= 60) {
-        return "Grade E";
-    }
-    else
-        return "Sorry! you are fail";
+  if (num >= 90) {
+    return "Grade A";
+  } else if (num >= 80 && num < 90) {
+    return "Grade B";
+  } else if (num >= 70 && num < 80) {
+    return "Grade C";
+  } else if (num >= 60 && num <= 70) {
+    return "Grade D";
+  } else if (num >= 50 && num <= 60) {
+    return "Grade E";
+  } else return "Sorry! you are fail";
 }
 
 console.log(score(85));
