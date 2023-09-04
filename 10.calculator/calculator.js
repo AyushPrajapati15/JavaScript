d963f603c62888923aafa82237325503e74730c6
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-09-04 22:05:44 +0530

    simple calculator program

---
+++ 10.calculator/calculator.js
@@ -0,0 +1,33 @@
+function add(x, y) {
+    console.log("The first number is ", x);
+    console.log("The second number is", y);
+    console.log("The sum of x and y is", x + y);
+    console.log("\n");
+    
+}
+function sub(x, y) {
+    console.log("The first number is ", x);
+    console.log("The second number is", y);
+    console.log("The sum of x and y is", x - y);
+    console.log("\n");
+    
+}
+function mul(x, y) {
+    console.log("The first number is ", x);
+    console.log("The second number is", y);
+    console.log("The sum of x and y is", x * y);
+    console.log("\n");
+    
+}
+function div(x, y) {
+    console.log("The first number is ", x);
+    console.log("The second number is", y);
+    console.log("The sum of x and y is", x / y);
+    console.log("\n");
+    
+}
+
+add(10, 25);
+sub(20, 15);
+mul(50, 10);
+div(80, 10);
