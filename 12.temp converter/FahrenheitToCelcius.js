9e0acac351d8ec32f03cb5be1f533ad9798ef2e1
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-09-06 22:16:22 +0530

    Fahrenheit to Celcius

--- 12.temp converter/FahrenheitToCelcius.js
+++ 12.temp converter/FahrenheitToCelcius.js
@@ -1,6 +1,10 @@
+// Fahrenheit to celcius
+
 function fahrenheit(fah) {
     //Formula to calculate Fahrenheit to celcius--> C = (F - 32) × 5/9
+
     let cel = (fah - 32) * 5 / 9;
+
     console.log(cel);
 }
 fahrenheit(212);
