01063ce119cd8001d9d639bac21439b835eea578
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-09-07 22:32:26 +0530

    smallest element in array

--- 13.Smallest array/min array.js
+++ 13.Smallest array/min array.js
@@ -1,15 +1,14 @@
 // program to find the smallest element in an array.
 function small(arr) {
-    let smallest = arr[0];
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] < smallest) {
-        smallest = arr[i];
-      }
+  let smallest = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
     }
-    return smallest;
   }
-  
-  let arr = [1, 5, 2, 4, 3, 8, 9];
-  // large(arr);
-  console.log(small(arr));
-  
+  return smallest;
+}
+
+let arr = [1, 5, 2, 4, 3, 8, 9];
+// large(arr);
+console.log(small(arr));
