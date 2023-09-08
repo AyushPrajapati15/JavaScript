534be68721064cb925cac7b06e2a405bf126a18f
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-09-08 22:31:54 +0530

    Common elements in an array

---
+++ 14.Common Element/commonElement.js
@@ -0,0 +1,40 @@
+// function findCommonElements(arr1, arr2) {
+//     const commonElements = [];
+  
+//     for (let element of arr1) {
+//       if (arr2.includes(element) && !commonElements.includes(element)) {
+//         commonElements.push(element);
+//       }
+//     }
+  
+//     return commonElements;
+//   }
+  
+//   // Example usage:
+//   const array1 = [1, 2, 3, 4, 5];
+//   const array2 = [3, 4, 5, 6, 7];
+//   const common = findCommonElements(array1, array2);
+//   console.log(common); // Outputs: [3, 4, 5]
+  
+
+
+function findCommonElements(arr1, arr2) {
+    const commonElements = [];
+  
+    for (let i = 0; i < arr1.length; i++) {
+      for (let j = 0; j < arr2.length; j++) {
+        if (arr1[i] === arr2[j] && !commonElements.includes(arr1[i])) {
+          commonElements.push(arr1[i]);
+        }
+      }
+    }
+  
+    return commonElements;
+  }
+  
+  // Example usage:
+  const array1 = [1, 2, 3, 4, 5];
+  const array2 = [3, 4, 5, 6, 7];
+  const common = findCommonElements(array1, array2);
+  console.log(common); // Outputs: [3, 4, 5]
+  
