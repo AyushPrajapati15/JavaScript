b09fee2078eab2ffff6d05df71ea0bdca0c3d196
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-09-09 21:19:53 +0530

    power calculator

---
+++ 15.ayu/powerCalcusing2.js
@@ -0,0 +1,10 @@
+function calculatePower(base, exponent) {
+    return base ** exponent;
+  }
+  
+  // Example usage:
+  const base = 2;
+  const exponent = 3;
+  const result = calculatePower(base, exponent);
+  console.log(`${base} to the power of ${exponent} is ${result}`); // Outputs: 2 to the power of 3 is 8
+  
