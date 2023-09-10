10d93b356f79c7e6d737a1b4b8d4462d6c705348
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-09-10 22:12:39 +0530

    Bubble Sort

---
+++ 16.Sorting/Bubble Sort.js
@@ -0,0 +1,26 @@
+function sort(arr) {
+    let temp = 0;
+    let swapped;
+
+    for (let i = 0; i < arr.length - 1; i++){
+        swapped = false;
+        for (let j = 0; j < arr.length - 1 - i; j++){
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            break;
+        }
+    }
+    return arr;
+    
+}
+
+
+let arr = [5, 4, 6, 8, 2, 9, 3, 7];
+let sorted = sort(arr);
+console.log(sorted);
