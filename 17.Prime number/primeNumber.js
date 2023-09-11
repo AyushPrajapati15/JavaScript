5f5066db034a24456a77b56aadad47d185249681
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-09-11 22:33:57 +0530

    Prime number

--- 17.Prime number/primeNumber.js
+++ 17.Prime number/primeNumber.js
@@ -1,20 +1,20 @@
 function isPrime(num) {
-    // Check if the number is less than 2 (0 and 1 are not prime)
+    
     if (num < 2) {
       return false;
     }
   
-    // Check for divisibility from 2 to the square root of the number
+
     for (let i = 2; i <= Math.sqrt(num); i++) {
       if (num % i === 0) {
-        return false; // It's divisible by i, so it's not prime
+        return false; 
       }
     }
   
-    return true; // It's not divisible by any number other than 1 and itself, so it's prime
+    return true; 
   }
   
-  // Example usage:
+  
   const number = 17;
   if (isPrime(number)) {
     console.log(`${number} is a prime number.`);
