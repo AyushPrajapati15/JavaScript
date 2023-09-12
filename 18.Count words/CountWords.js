74087e63bfea90bc95ee8cb10bf830a8b51bcdee
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-09-12 22:27:22 +0530

    Count words

---
+++ 18.Count words/CountWords.js
@@ -0,0 +1,26 @@
+function countWords(sentence) {
+  let wordCount = 0;
+  let isInsideWord = false;
+
+  for (let i = 0; i < sentence.length; i++) {
+    const char = sentence[i];
+
+
+    if (/[a-zA-Z0-9]/.test(char)) {
+      if (!isInsideWord) {
+
+        wordCount++;
+        isInsideWord = true;
+      }
+    } else {
+      isInsideWord = false;
+    }
+  }
+
+  return wordCount;
+}
+
+
+const inputSentence = "This is a sample sentence with several words.";
+const wordCount = countWords(inputSentence);
+console.log(`Word count: ${wordCount}`);
