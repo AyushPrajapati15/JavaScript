92a87faea431085f8097943411a43b4d48eed186
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-09-13 23:37:39 +0530

    Missing number

---
+++ 19.missing number/MissingNumber.js
@@ -0,0 +1,20 @@
+function findMissingNumber(nums) {
+    const n = nums.length;
+
+    const expectedSum = (n * (n + 1)) / 2;
+
+    let actualSum = 0;
+  
+    for (let i = 0; i < n; i++) {
+      actualSum += nums[i];
+    }
+  
+
+    return expectedSum - actualSum;
+  }
+  
+  // Example usage:
+  const nums = [3, 0, 1];
+  const missingNumber = findMissingNumber(nums);
+  console.log(`The missing number is: ${missingNumber}`);
+  
