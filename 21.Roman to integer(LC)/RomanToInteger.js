f0e32ca8895ffc3d8067da6df9fcaaab8e70877e
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-09-17 23:20:33 +0530

    Roman to integer

---
+++ 21.Roman to integer(LC)/RomanToInteger.js
@@ -0,0 +1,30 @@
+function romanToInt(s) {
+    const romanToIntegerMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+    let prevValue = 0;
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        const currentValue = romanToIntegerMap[s[i]];
+        if (currentValue >= prevValue) {
+            result += currentValue;
+        } else {
+            result -= currentValue;
+        }
+        prevValue = currentValue;
+    }
+
+    return result;
+}
+
+const romanNumeral = "MCMXCIV"; // Example Roman numeral: 1994
+const result = romanToInt(romanNumeral);
+console.log(`The integer value of ${romanNumeral} is: ${result}`);
