50d7f7a944ab77967407b6ac8e57672b9f8a43bd
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-09-26 21:49:00 +0530

    Triangle pattern

--- 25.Pattern2/trianglePatternIncresing.js
+++ 25.Pattern2/trianglePatternIncresing.js
@@ -1,9 +1,10 @@
+// triangle pattern incresing
 let n = 4;
 let string = "";
 let count = 1;
-// External loop
+
 for (let i = 1; i <= n; i++) {
-  // Internal loop
+    
   for (let j = 1; j <= i; j++) {
     string += count;
     count++;
