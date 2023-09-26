28dce29c1bb16ddb94f1e6bbe6c6616ebe651309
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-09-26 21:47:28 +0530

    Triangle pattern

---
+++ 25.Pattern2/trianglePattern.js
@@ -0,0 +1,11 @@
+let n = 5; // height of pattern
+let string = "";
+// External loop
+for (let i = 1; i <= n; i++) {
+  // Internal loop
+  for (let j = 1; j <= i; j++) {
+    string += i;
+  }
+  string += "\n";
+}
+console.log(string);
