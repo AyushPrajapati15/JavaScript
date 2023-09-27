9125397501fe225234b02f3bc841e6e6fd718888
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-09-27 22:17:58 +0530

    Reverse triangle pattern

--- 26.pattern3/ReverseTriangle1.js
+++ 26.pattern3/ReverseTriangle1.js
@@ -1,6 +1,7 @@
-let n = 5; // height of pattern
+// Reverse triangle pattern
+let n = 5;
 let string = "";
-// External loop
+
 for (let i = 1; i <= n; i++) {
   for (let j = 1; j <= n - i + 1; j++) {
     string += n - j + 1;
