d0a4d441b7e2d1853bc450f8da9e31ac58893320
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-09-29 17:42:43 +0530

    Increasing number Triangle pattern

---
+++ 28.Pattern/IncreasingTriangle.js
@@ -0,0 +1,18 @@
+// Increasing number Triangle pattern
+let n = 3;
+let count = 1;
+let string = "";
+
+for (let i = 1; i <= n; i++) {
+  
+  for (let j = 1; j <= n - i; j++) {
+    string += " ";
+  }
+  
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    string += count;
+    count++;
+  }
+  string += "\n";
+}
+console.log(string);
