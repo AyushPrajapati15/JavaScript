25398b3efaedee2534c94a437b974b9c7971fa6b
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-10-01 22:12:30 +0530

    Hourglass Pattern

--- 30.Pattern7/Hourglass Pattern.js
+++ 30.Pattern7/Hourglass Pattern.js
@@ -1,6 +1,7 @@
+// Hourglass Pattern
 let n = 5;
 let string = "";
-// Reverse Pyramid
+
 for (let i = 1; i <= n; i++) {
   for (let j = 1; j < i; j++) {
     string += " ";
@@ -10,7 +11,7 @@ for (let i = 1; i <= n; i++) {
   }
   string += "\n";
 }
-// Pyramid
+
 for (let i = 1; i <= n - 1; i++) {
   for (let j = 1; j < n - i; j++) {
     string += " ";
