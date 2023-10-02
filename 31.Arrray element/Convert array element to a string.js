0767e15fb8e005105592ac0ffe8adbfd3aa9e1ac
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-10-02 22:57:20 +0530

    Convert array element to a string

---
+++ 31.Arrray element/Convert array element to a string.js
@@ -0,0 +1,11 @@
+// JavaScript array element to string
+var arr = [10, 25, true, 'Hello'];
+
+console.log(arr[0].toString()); 
+console.log(`${arr[1]}`); 
+console.log(String(arr[2])); 
+console.log(arr[1] + ''); 
+
+
+console.log(typeof arr[0].toString()); 
+console.log(typeof `${arr[1]}`); 
