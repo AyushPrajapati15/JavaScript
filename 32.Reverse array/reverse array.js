70daef5c9f033a49f6b45b0c2d20a7aab1828d5b
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-10-03 23:16:44 +0530

    Reverse array

---
+++ 32.Reverse array/reverse array.js
@@ -0,0 +1,18 @@
+function reverseArray(arr) {
+    const length = arr.length;
+    const middle = Math.floor(length / 2);
+  
+    for (let i = 0; i < middle; i++) {
+      const temp = arr[i];
+      arr[i] = arr[length - 1 - i];
+      arr[length - 1 - i] = temp;
+    }
+  
+    return arr;
+  }
+  
+  // Example usage
+  const arr = [1, 2, 3, 4, 5];
+  reverseArray(arr);
+  console.log(arr);  // Output: [5, 4, 3, 2, 1]
+  
