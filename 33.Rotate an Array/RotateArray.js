c18218d690fa2a7e7e55307af79060d1b7e33a24
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-10-04 23:09:46 +0530

    Rotate an array

--- 33.Rotate an Array/RotateArray.js
+++ 33.Rotate an Array/RotateArray.js
@@ -1,18 +1,17 @@
 // Rotate an array
 function rotateArray(arr, steps) {
-    if (steps < 0) {
-        
-      steps = steps + arr.length;
-    }
-    
-    steps = steps % arr.length;
-    
-    return arr.slice(-steps).concat(arr.slice(0, arr.length - steps));
+  if (steps < 0) {
+    steps = steps + arr.length;
   }
-  
-  
-  const arr = [1, 2, 3, 4, 5];
-  const steps = 2;
-  const rotatedArray = rotateArray(arr, steps);
-  
-  console.log(rotatedArray);
+
+  steps = steps % arr.length;
+
+  return arr.slice(-steps).concat(arr.slice(0, arr.length - steps));
+}
+
+const arr = [1, 2, 3, 4, 5];
+console.log(arr);
+const steps = 2;
+const rotatedArray = rotateArray(arr, steps);
+
+console.log(rotatedArray);
