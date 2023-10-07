89b42987f872f2e0d9cdd882ea9cdf8e4979ffe4
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-10-07 23:13:48 +0530

    Loop Through Array Of Objects And Delete Filter

---
+++ 35.Loop Through Array Of Objects And Delete Filter/Loop Through Array Of Objects And Delete Filter.js
@@ -0,0 +1,15 @@
+// Loop Through Array Of Objects And Delete Filter
+const data = [
+    { name: 'John', age: 30, group: 'A' },
+    { name: 'Mary', age: 25, group: 'B' },
+    { name: 'Mike', age: 20, group: 'A' },
+    { name: 'Jane', age: 15, group: 'C' },
+    { name: 'Peter', age: 25, group: 'B' }
+  ];
+  
+  // filter out people whose age is greater than 20
+  const filteredData = data.filter(function(item) {
+    return item.age > 20;
+  });
+  
+  console.log(filteredData);
